608580fcd5f81ea2118c7ca9cd53ee6708b273d3
Niklas Lausch <niki@lauschig.net>
2023-06-22 23:56:38 +0200

    Added AUTH_SECRET env

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,6 +1,7 @@
 import { NuxtAuthHandler } from '#auth'
 import TwitchProvider from 'next-auth/providers/twitch'
 export default NuxtAuthHandler({
+  secret: process.env.AUTH_SECRET,
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     TwitchProvider.default({
