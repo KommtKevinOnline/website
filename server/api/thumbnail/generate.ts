15a3460540b20526404eb371af3c7e89ebd0834b
Niklas Lausch <niki@lauschig.net>
2023-07-09 14:11:18 +0200

    Added error handling in backend

--- server/api/thumbnail/generate.ts
+++ server/api/thumbnail/generate.ts
@@ -1,23 +1,28 @@
 import sharp from "sharp";
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  const { originalThumbnail, overlayId, flipX } = body
+  try {
+    const body = await readBody(event)
+    const { originalThumbnail, overlayId, flipX } = body
 
-  const thumbnail = await fetch(originalThumbnail);
-  const overlay = await fetch(`${process.env.AUTH_ORIGIN}/overlays/${overlayId}.png`);
+    const thumbnail = await fetch(originalThumbnail);
+    const overlay = await fetch(`${process.env.AUTH_ORIGIN}/overlays/${overlayId}.png`);
 
-  const overlayImg = sharp(await overlay.arrayBuffer()).flop(flipX)
-  const metadata = await overlayImg.metadata()
+    const overlayImg = sharp(await overlay.arrayBuffer()).flop(flipX)
+    const metadata = await overlayImg.metadata()
 
-  const image = await sharp(await thumbnail.arrayBuffer())
-    .resize(metadata.width, metadata.height)
-    .composite([{ 
-      input: await overlayImg.toBuffer()
-    }])
-  .toBuffer()
+    const image = await sharp(await thumbnail.arrayBuffer())
+      .resize(metadata.width, metadata.height)
+      .composite([{ 
+        input: await overlayImg.toBuffer()
+      }])
+    .toBuffer()
 
-  setHeader(event, 'Content-Type', 'image/jpg')
+    setHeader(event, 'Content-Type', 'image/jpg')
 
-  return image
+    return image
+  } catch (error) {
+    console.error(error)
+    return createError(error)
+  }
 })
