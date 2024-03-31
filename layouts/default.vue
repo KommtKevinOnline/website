915731d7baabaae62dc5393afa69e72b7f5ed51d
Niklas Lausch <niki@lauschig.net>
2024-03-31 17:21:48 +0200

    feat: added analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,19 @@
 
 <script setup lang="ts">
 import { routeItems } from "../statics/route-items";
+
+useHead({
+  script: [
+    {
+      src: "https://analytics.niki2k1.dev/js/script.js",
+      defer: true,
+      "data-domain": "kommtkevinonline.de",
+    },
+  ],
+  htmlAttrs: {
+    lang: "de",
+  },
+});
 </script>
 
 <style scoped>
