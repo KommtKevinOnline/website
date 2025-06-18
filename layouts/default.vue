3d2f8354d8873eb46b38af645e41df989db29bc2
Niklas Lausch <niki@lauschig.net>
2025-06-18 17:31:18 +0200

    feat: info banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,11 @@
 <template>
+  <div
+    class="bg-orange-500 py-4 text-center text-white font-semibold flex justify-center gap-2"
+  >
+    <UIcon name="i-heroicons-information-circle" class="size-6" />
+    Kevin streamt aktuell unregelmäßig, daher kann KommtKevinOnline falsche
+    Daten anzeigen. Informiert euch lieber über Whatsapp oder Instagram.
+  </div>
   <Header />
 
   <UMain>
