cadc14a658f3116995f6b61b30a45a1a36aa1a79
Niklas Lausch <niki@lauschig.net>
2025-12-09 14:10:18 +0100

    fix: deactivate info

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
   <div
+    v-if="false"
     class="bg-orange-500 py-4 text-center text-white font-semibold flex justify-center gap-2"
   >
     <UIcon name="i-heroicons-information-circle" class="size-6" />
-    Kevin macht die Restliche Woche was mit seiner Family. Der Nächste Stream ist am Montag den 1. Dezember.
+    Kevin macht die Restliche Woche was mit seiner Family. Der Nächste Stream
+    ist am Montag den 1. Dezember.
   </div>
   <Header />
 
